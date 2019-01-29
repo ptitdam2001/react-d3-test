d0af0ef5dd4b886ab2425f4bb53a1812e5f86978
Damien Suhard <damien.suhard@oveasoft.com>
2019-01-29 18:01:38 +0100

    main component

--- src/main/main.component.jsx
+++ src/main/main.component.jsx
@@ -3,12 +3,14 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import { Board } from '../tictactoe/board';
 import { Chart1 } from '../chart1/chart1';
 
+import './main.css';
+
 export class Main extends Component {
     render() {
         return (
             <Router>
-                <div>
-                    <ul>
+                <div className="content">
+                    <ul className="menu">
                         <li>
                             <Link to="/tictactoe">Tic tac toe</Link>
                         </li>
@@ -17,8 +19,10 @@ export class Main extends Component {
                         </li>
                     </ul>
                     <hr />
-                    <Route path="/tictactoe" component={Board} />
-                    <Route path="/chart" component={Chart1} />
+                    <div className="content">
+                        <Route path="/tictactoe" component={Board} />
+                        <Route path="/chart" component={Chart1} />
+                    </div>
                 </div>
             </Router>
         );
