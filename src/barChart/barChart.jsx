98f82ef3e7c7ea4281896e6a828ea3ebabdac384
Damien Suhard <damien.suhard@oveasoft.com>
2019-01-31 18:04:42 +0100

    fix barchart

--- src/barChart/barChart.jsx
+++ src/barChart/barChart.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 
 import {max} from 'd3-array';
-import {scaleLinear} from 'd3-scale';
+import { scaleLinear, scaleBand } from 'd3-scale';
 import { select } from 'd3-selection';
+import {axisLeft, axisBottom} from 'd3-axis';
 
 import './barChart.css';
 
@@ -10,6 +11,7 @@ export class BarChart extends Component {
   constructor(props) {
     super(props);
     this.createBarChart = this.createBarChart.bind(this);
+    this.margin = { top: 20, right: 20, bottom: 30, left: 40 };
   }
 
   componentDidMount() {
@@ -22,35 +24,72 @@ export class BarChart extends Component {
 
   createBarChart() {
     const node = this.node;
-    const dataMax = max(this.props.data);
-    const yScale = scaleLinear()
-      .domain([0, dataMax])
-      .range([0, this.props.size[1]]);
 
-    select(node)
-      .selectAll("rect")
+    const height = this.props.size[1] - this.margin.top - this.margin.bottom;
+    const width = this.props.size[0] - this.margin.left - this.margin.right;
+
+    const x = scaleBand().range([0, width]).padding(0.1);
+    const y = scaleLinear().range([height, 0]);
+
+    const svg = select(node)
+      .attr('width', width + this.margin.left + this.margin.right)
+      .attr('height', height + this.margin.top + this.margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.right + ')');
+
+
+
+    // select(node)
+    //   .selectAll("rect")
+    //   .data(this.props.data)
+    //   .enter()
+    //   .append("rect");
+
+    //   // .append('text')
+    //   // .text(d => d);
+
+    // select(node)
+    //   .selectAll("rect")
+    //   .data(this.props.data)
+    //   .exit()
+    //   .remove();
+
+    // select(node)
+    //   .selectAll('rect')
+    //   .data(this.props.data)
+    //   .style('fill', '#fe9922')
+    //   .attr('y', d => this.props.size[1] - yScale(d))
+    //   .attr('x', (d, i) => i * 25)
+    //   .attr('width', 25)
+    //   .attr('height', d => yScale(d))
+    //   .style('stroke', '#9a8b7a')
+    //   .style('stroke-width', '1px');
+
+    x.domain(this.props.data.map(d => d.name));
+    y.domain([0, max(this.props.data, d => d.votes + 200)]);
+
+    svg.selectAll('rect')
+      //.remove()
+      //.exit()
       .data(this.props.data)
       .enter()
-      .append("rect");
-      // .append('text')
-      // .text(d => d);
-
-    select(node)
-      .selectAll("rect")
-      .data(this.props.data)
-      .exit()
-      .remove();
-
-    select(node)
-      .selectAll('rect')
-      .data(this.props.data)
+      .append('rect')
       .style('fill', '#fe9922')
-      .attr('y', d => this.props.size[1] - yScale(d))
-      .attr('x', (d, i) => i * 25)
-      .attr('width', 25)
-      .attr('height', d => yScale(d))
-      .style('stroke', '#9a8b7a')
-      .style('stroke-width', '1px');
+      .attr('x', (data, idx) => x(data.name))
+      .attr('width', x.bandwidth())
+      .attr('y', d => y(d.votes))
+      .attr('height', d => height - y(d.votes))
+
+    svg
+      .append('g')
+      .attr('class', 'y-axis')
+      .call(axisLeft(y));
+
+    svg
+      .append('g')
+      .attr('class', 'x-axis')
+      .call(axisBottom(x))
+      .attr('transform', 'translate(0,'+ height +')');
   }
 
   render() {
--- src/main/mainBarChart.jsx
+++ src/main/mainBarChart.jsx
@@ -2,10 +2,36 @@ import React, { Component } from 'react'
 import { BarChart } from '../barChart/barChart';
 
 export class MainBarChart extends Component {
+  constructor(props) {
+    super(props);
+
+    this.rawData = [
+      {
+        name: 'Chelsea',
+        votes: 100,
+      },
+      {
+        name: 'Arsenal',
+        votes: 70,
+      },
+      {
+        name: 'Liverpool',
+        votes: 250,
+      },
+      {
+        name: 'Manchester City',
+        votes: 689,
+      },
+      {
+        name: 'Manchester United',
+        votes: 150,
+      },
+    ];
+  }
   render() {
     return (
       <div style={{padding: '10px'}}>
-        <BarChart data={[5, 10, 1, 3]} size={[500, 500]}></BarChart>
+        <BarChart data={this.rawData} size={[500, 500]}></BarChart>
       </div>
     )
   }
