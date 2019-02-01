c759b5bbc5823234f67128b37d4cf6b961bc0c30
Damien Suhard <damien.suhard@oveasoft.com>
2019-02-01 11:25:08 +0100

    finalize barchart

--- src/barChart/barChart.jsx
+++ src/barChart/barChart.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 import {max} from 'd3-array';
 import { scaleLinear, scaleBand } from 'd3-scale';
-import { select } from 'd3-selection';
+import { select, event } from 'd3-selection';
 import {axisLeft, axisBottom} from 'd3-axis';
 
 import './barChart.css';
@@ -12,89 +12,88 @@ export class BarChart extends Component {
     super(props);
     this.createBarChart = this.createBarChart.bind(this);
     this.margin = { top: 20, right: 20, bottom: 30, left: 40 };
+
+    this.svg = null;
   }
 
   componentDidMount() {
+    this._height = this.props.height - this.margin.top - this.margin.bottom;
+    this._width = this.props.width - this.margin.left - this.margin.right;
     this.createBarChart();
   }
 
   componentDidUpdate() {
-    this.createBarChart();
+    this.updateBar();
   }
 
   createBarChart() {
     const node = this.node;
 
-    const height = this.props.size[1] - this.margin.top - this.margin.bottom;
-    const width = this.props.size[0] - this.margin.left - this.margin.right;
+    this.x = scaleBand().range([0, this._width]).padding(0.1);
+    this.y = scaleLinear().range([this._height, 0]);
 
-    const x = scaleBand().range([0, width]).padding(0.1);
-    const y = scaleLinear().range([height, 0]);
-
-    const svg = select(node)
-      .attr('width', width + this.margin.left + this.margin.right)
-      .attr('height', height + this.margin.top + this.margin.bottom)
+    // init chart
+    this.svg = select(node)
+      .attr('width', this._width + this.margin.left + this.margin.right)
+      .attr('height', this._height + this.margin.top + this.margin.bottom)
       .append('g')
       .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.right + ')');
 
+    // init axis
+    this.svg
+      .append('g')
+      .attr('class', 'y-axis')
+      .call(axisLeft(this.y));
 
+    this.svg
+      .append('g')
+      .attr('class', 'x-axis')
+      .call(axisBottom(this.x))
+      .attr('transform', 'translate(0,'+ this._height +')');
 
-    // select(node)
-    //   .selectAll("rect")
-    //   .data(this.props.data)
-    //   .enter()
-    //   .append("rect");
+    this.updateBar();
+  }
 
-    //   // .append('text')
-    //   // .text(d => d);
+  updateBar() {
+    const x = this.x.domain(this.props.data.map(d => d.name));
+    const y = this.y.domain([0, max(this.props.data, d => d.votes + 200)]);
 
-    // select(node)
-    //   .selectAll("rect")
-    //   .data(this.props.data)
-    //   .exit()
-    //   .remove();
+    const tip = select(this.tip);
 
-    // select(node)
-    //   .selectAll('rect')
-    //   .data(this.props.data)
-    //   .style('fill', '#fe9922')
-    //   .attr('y', d => this.props.size[1] - yScale(d))
-    //   .attr('x', (d, i) => i * 25)
-    //   .attr('width', 25)
-    //   .attr('height', d => yScale(d))
-    //   .style('stroke', '#9a8b7a')
-    //   .style('stroke-width', '1px');
-
-    x.domain(this.props.data.map(d => d.name));
-    y.domain([0, max(this.props.data, d => d.votes + 200)]);
-
-    svg.selectAll('rect')
-      //.remove()
-      //.exit()
+    this.svg.selectAll('.bar')
+      .remove()
+      .exit()
       .data(this.props.data)
       .enter()
       .append('rect')
+      .attr('class', 'bar')
       .style('fill', '#fe9922')
       .attr('x', (data, idx) => x(data.name))
       .attr('width', x.bandwidth())
       .attr('y', d => y(d.votes))
-      .attr('height', d => height - y(d.votes))
+      .attr('height', d => this._height - y(d.votes))
+      .on('mousemove', data => {
+        tip.style('position', 'absolute')
+          .style('left', `${event.pageX + 10}px`)
+          .style('top', `${event.pageY + 20}px`)
+          .style('display', 'inline-block')
+          .style('opacity', '0.9')
+          .html(`<div><strong>${data.name}</strong>: ${data.votes} votes</div>`)
+      })
+      .on('mouseout', () => tip.style('display', 'none'));
 
-    svg
-      .append('g')
-      .attr('class', 'y-axis')
-      .call(axisLeft(y));
-
-    svg
-      .append('g')
-      .attr('class', 'x-axis')
-      .call(axisBottom(x))
-      .attr('transform', 'translate(0,'+ height +')');
+    // update axis
+    this.svg.select('.x-axis').call(axisBottom(x));
+    this.svg.select('.y-axis').call(axisLeft(y));
   }
 
   render() {
     return (
-      <svg ref={node => this.node = node} width={500} height={500}></svg>
+      <div>
+        <svg ref={node => this.node = node} width={this.props.width} height={this.props.height}></svg>
+        <div ref={node => this.tip = node} className="tooltip"></div>
+        {this.props.children}
+      </div>
     )
   }
 }
--- src/main/mainBarChart.jsx
+++ src/main/mainBarChart.jsx
@@ -5,34 +5,50 @@ export class MainBarChart extends Component {
   constructor(props) {
     super(props);
 
-    this.rawData = [
-      {
-        name: 'Chelsea',
-        votes: 100,
-      },
-      {
-        name: 'Arsenal',
-        votes: 70,
-      },
-      {
-        name: 'Liverpool',
-        votes: 250,
-      },
-      {
-        name: 'Manchester City',
-        votes: 689,
-      },
-      {
-        name: 'Manchester United',
-        votes: 150,
-      },
-    ];
+    this.state = {
+      'rawData': [
+        {
+          name: 'Chelsea',
+          votes: 100,
+        },
+        {
+          name: 'Arsenal',
+          votes: 70,
+        },
+        {
+          name: 'Liverpool',
+          votes: 250,
+        },
+        {
+          name: 'Manchester City',
+          votes: 689,
+        },
+        {
+          name: 'Manchester United',
+          votes: 150,
+        },
+      ]
+    };
   }
   render() {
+    const rawData = this.state.rawData;
+
     return (
       <div style={{padding: '10px'}}>
-        <BarChart data={this.rawData} size={[500, 500]}></BarChart>
+        <BarChart data={rawData} height={500} width={500}>
+          <div>transclusion here</div>
+        </BarChart>
+        <button onClick={() => this.randomData()}>Generate</button>
       </div>
     )
+  }
+
+  randomData() {
+    this.setState({
+      'rawData': this.state.rawData.map(item => {
+        item.votes = Math.floor((Math.random() * 100000) + 1);
+        return item;
+      })
+    });
   }
 }
